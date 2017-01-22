7c7b115d2b97263f9cd8c8d70febc788c71efdb5
Victor Macias <v.maciascastillo@gmail.com>
2017-01-22 22:39:40 +0100

    Add navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
+require 'httparty'
 class PagesController < ApplicationController
   def home
+  	# @response = HTTParty.get('https://vic0707.carto.com/api/v2/sql?q=select%20*%20from%20public.asia_adm0%20limit%2010')
+  end
+
+  def load_data
+
   end
 end
