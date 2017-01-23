af2087c5c1a7b958088ee6cf9c06e9c8470c3918
Victor Macias <v.maciascastillo@gmail.com>
2017-01-24 00:14:20 +0100

    Add private method render_country

--- app/controllers/cartoapi_controller.rb
+++ app/controllers/cartoapi_controller.rb
@@ -18,21 +18,27 @@ class CartoapiController < ApplicationController
 			@response = api_call( @username, @table, @page)
 		end
 		if  @response.first[0] != "error"
-			if @table.include? "asia"
-				render "cartoapi/asia", locals: {response: @response, page: @page, username: @username, table: @table}
-			elsif @table.include? "africa"
-				render "cartoapi/africa", locals: {response: @response, page: @page, username: @username, table: @table}
-			elsif @table.include? "europe"
-				render "cartoapi/europe", locals: {response: @response, page: @page, username: @username, table: @table}
-			elsif @table.include? "samerica"
-				render "cartoapi/samerica", locals: {response: @response, page: @page, username: @username, table: @table}
-			elsif @table.include? "oceania"
-				render "cartoapi/oceania", locals: {response: @response, page: @page, username: @username, table: @table}
-			else
-				render "cartoapi/namerica", locals: {response: @response, page: @page, username: @username, table: @table}
-			end
+			render_country(@response, @page, @username, @table)
 		else
 			redirect_to root_path
+		end
+	end
+
+	private
+
+	def render_country(response, page, username, table)
+		if table.include? "asia"
+			render "cartoapi/asia", locals: {response: response, page: page, username: username, table: table}
+		elsif table.include? "africa"
+			render "cartoapi/africa", locals: {response: response, page: page, username: username, table: table}
+		elsif table.include? "europe"
+			render "cartoapi/europe", locals: {response: response, page: page, username: username, table: table}
+		elsif table.include? "samerica"
+			render "cartoapi/samerica", locals: {response: response, page: page, username: username, table: table}
+		elsif table.include? "oceania"
+			render "cartoapi/oceania", locals: {response: response, page: page, username: username, table: table}
+		else
+			render "cartoapi/namerica", locals: {response: response, page: page, username: username, table: table}
 		end
 	end
 end
